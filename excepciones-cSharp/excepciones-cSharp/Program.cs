70be9b1df2ba85d71ca2c1421ca78e6435665c74
cristianHernandezVITT <105743030+cristianHernandezVITT@users.noreply.github.com>
2022-05-19 11:51:38 -0700

    Add files via upload

---
+++ excepciones-cSharp/excepciones-cSharp/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace excepciones_cSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Introduce un número: ");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Introduce otro número: ");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine(num1 / num2);
+                Console.ReadLine();
+            }
+            catch(DivideByZeroException e)
+            {
+                Console.Write(e.Message);
+                Console.ReadLine();
+            }
+            catch (FormatException e)
+            {
+                Console.Write(e.Message);
+                Console.ReadLine();
+            }
+            catch(OverflowException e)
+            {
+                Console.Write(e.Message);
+                Console.ReadLine();
+            }
+            finally
+            {
+
+            }
+        }
+    }
+}
